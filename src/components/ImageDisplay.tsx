53dc39a504b8ff3b24c1905507cee44f5735e585
sagara_rikuto <sagara_rikuto@plise.co.jp>
2024-05-04 01:24:59 +0900

    fix

--- src/components/ImageDisplay.tsx
+++ src/components/ImageDisplay.tsx
@@ -16,7 +16,7 @@ const ImageDisplay = ({ showFlagItem, answerFlagIds }: ImageDisplayProps) => {
     <div>
       <div className="mb-4">
         <label className="inline-flex items-center me-5 cursor-pointer">
-          <span className="ms-3 mx-2 text-sm font-medium">回答表示</span>
+          <span className="ms-3 mx-2 text-sm font-medium">解答表示</span>
           <input 
             type="checkbox" 
             value="" 
@@ -27,7 +27,7 @@ const ImageDisplay = ({ showFlagItem, answerFlagIds }: ImageDisplayProps) => {
           <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
         </label>
       </div>
-      <div className="flex flex-wrap justify-center" style={{width: '90%' , margin: '0 auto'}}>
+      <div className="flex flex-wrap justify-center" style={{width: '100%' , margin: '0 auto'}}>
         {Array.from({ length: 100 }, (_, i) => i + 1).map((id) => {
           const item = showFlagItem.find((item) => item[0] === id);
 
@@ -47,8 +47,8 @@ const ImageDisplay = ({ showFlagItem, answerFlagIds }: ImageDisplayProps) => {
                     <Image
                       src={`/img/quiz/${item[4]}`}
                       alt={item[1]}
-                      width={250}
-                      height={250}
+                      width={350}
+                      height={350}
                     />
                   </Link>
                   {showAnswer && item[3] === 1 && <p>{item[1]}</p>}
@@ -57,8 +57,8 @@ const ImageDisplay = ({ showFlagItem, answerFlagIds }: ImageDisplayProps) => {
                 <Image
                   src="/img/sample.png"
                   alt="Sample Image"
-                  width={250}
-                  height={250}
+                  width={350}
+                  height={350}
                 />
               )}
             </div>
