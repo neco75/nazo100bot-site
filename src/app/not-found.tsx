93c14d2ab7ef8909c61697236f17d0e1ddc2df15
sagara_rikuto <sagara_rikuto@plise.co.jp>
2024-05-06 21:06:39 +0900

    fix not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,9 +3,12 @@ import Link from 'next/link'
 const NotFound = () => {
     return (
       <div className="container mx-auto py-[50px]">
-        <Link href="/guideline">
+       
           <h2 className="text-[50px] text-center">404</h2>
-        </Link>
+          <h2 className="text-[40px] leading-tight text-center">
+            <div>NOT  <Link href="/guideline">FOUND</Link></div>
+          </h2>
+          <div className="text-center mt-4">このページは存在しません。</div>
       </div>
     )
   }
